010df6815d1aca1409ab56bfc5ba51d9a0506d61
ahmadariff990 <ahmadariff990@gmail.com>
2026-07-04 03:21:08 +0000

    Add shared NETCONF connection config

---
+++ netconf_config.py
@@ -0,0 +1,8 @@
+DEVICE = {
+    "host": "10.10.20.48",
+    "port": 830,
+    "username": "developer",
+    "password": "C1sco12345",
+    "hostkey_verify": False,
+    "device_params": {"name": "csr"},
+}
